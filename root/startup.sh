a3859c77a64bc10bd2ea94904eea13ba11803429
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-01-29 18:24:46 -0800

    Fix network shares

--- root/startup.sh
+++ root/startup.sh
@@ -1,8 +1,19 @@
 #! /bin/sh
 
-trap "umount -t cifs -a; killall sleep; exit" TERM INT 
+trap "umount -t cifs -a; killall sleep; exit" TERM INT
 
-sh /mount.sh
+while read LINE; do
+  SHARE=$(echo $LINE | cut -d ',' -f1)
+  LOCAL=$(echo $LINE | cut -d ',' -f2)
+  USERNAME=$(echo $LINE | cut -d ',' -f3)
+  PASSWORD=$(echo $LINE | cut -d ',' -f4)
+  mkdir -p "/folders/${LOCAL}"
+  if [ "${USERNAME}" != "" -a "${PASSWORD}" != "" ]; then
+    mount.cifs "${SHARE}" "/folders/${LOCAL}" -o "uid=root,gid=root,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm,username=${USERNAME},password=${PASSWORD}" &
+  else
+    mount.cifs "${SHARE}" "/folders/${LOCAL}" -o "uid=root,gid=root,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm,guest" &
+  fi
+done < /mounts
 
 sleep 2147483647d &
 wait "$!"
